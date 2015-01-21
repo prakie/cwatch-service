8f4b137f989adeca21096e58d3a246a25a1f20a9
maprohu <maprohu@gmail.com>
2015-01-21 15:18:54 +0000

    fixing activemq

--- core/src/main/java/org/cwatch/service/routes/CwatchRouteBuilder.java
+++ core/src/main/java/org/cwatch/service/routes/CwatchRouteBuilder.java
@@ -4,7 +4,6 @@ import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.activemq.camel.component.ActiveMQConfiguration;
-import org.apache.activemq.spring.ActiveMQConnectionFactory;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.cwatch.vdm.StiresSender;
@@ -24,26 +23,39 @@ public class CwatchRouteBuilder extends SpringRouteBuilder {
 	
 	@Override
 	public void configure() throws Exception {
+		errorHandler(
+				defaultErrorHandler()
+				.logStackTrace(false)
+				.logRetryStackTrace(false)
+				.logExhaustedMessageHistory(false)
+		);
+		
 		from("servlet:///" + batchServicePath + "?servletName=stiresVdmCoreServlet")
 		.id("batchServlet")
+		.startupOrder(3)
 		.to(ExchangePattern.InOnly, "activemq:topic:stires.vdm.batch");
 		
 		from("servlet:///" + monitoringServicePath+ "?servletName=stiresVdmCoreServlet")
 		.id("monitoringServlet")
+		.startupOrder(4)
 		.to(ExchangePattern.InOnly, "activemq:topic:stires.vdm.monitoring");
 		
-		from("activemq:topic:stires.vdm.batch")
+		from("activemq:topic:stires.vdm.batch?clientId=cwatchServiceStiresSenderBatch&asyncConsumer=true")
 		.id("batchSender")
+		.startupOrder(1)
+		.threads()
 		.to("bean:stiresSender?method=sendMessages(${in.header.SSNPRoxy}, ${body})");
 		
-		from("activemq:topic:stires.vdm.monitoring")
+		from("activemq:topic:stires.vdm.monitoring?clientId=cwatchServiceStiresSenderMonitoring&asyncConsumer=true")
 		.id("monitoringSender")
+		.startupOrder(2)
+		.threads()
 		.to("bean:stiresSender?method=sendMonitoring(${in.header.SSNPRoxy}, ${body})");
 	}
 	
 	@Bean
 	BrokerService cwatchServiceBroker() throws Exception {
-		return BrokerFactory.createBroker("broker:(tcp://localhost:61616)?brokerName=cwatchServiceBroker&persistent=false", true);
+		return BrokerFactory.createBroker("broker:(tcp://0.0.0.0:61616)?brokerName=cwatchServiceBroker&persistent=false&useShutdownHook=false");
 	}
 	
 	@Bean
@@ -51,7 +63,10 @@ public class CwatchRouteBuilder extends SpringRouteBuilder {
 	ActiveMQComponent activemq() {
 		ActiveMQConfiguration cfg = new ActiveMQConfiguration();
 		cfg.setBrokerURL("vm://cwatchServiceBroker?create=false");
-		return new ActiveMQComponent(cfg);
+		
+		ActiveMQComponent cmp = new ActiveMQComponent();
+		cmp.setConfiguration(cfg);
+		return cmp;
 	}
 	
 	@Bean
