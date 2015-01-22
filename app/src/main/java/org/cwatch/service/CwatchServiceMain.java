7e85b6ef068d6d5cd80a1aa215fd164e33e4003d
maprohu <maprohu@gmail.com>
2015-01-22 20:00:58 +0000

    adding app

--- app/src/main/java/org/cwatch/service/CwatchServiceMain.java
+++ app/src/main/java/org/cwatch/service/CwatchServiceMain.java
@@ -1,20 +1,22 @@
 package org.cwatch.service;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.activemq.camel.component.ActiveMQConfiguration;
 import org.apache.camel.model.ContextScanDefinition;
 import org.apache.camel.spring.CamelContextFactoryBean;
-import org.apache.camel.spring.Main;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.cwatch.split.CwatchSplitConfiguration;
+import org.cwatch.split.routes.CwatchSplitRouteBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({CwatchSplitConfiguration.class, CwatchSplitRouteBuilder.class})
 public class CwatchServiceMain implements CommandLineRunner {
-
-	@Autowired(required=true)
-	AbstractApplicationContext applicationContext;
 
 	@Bean
 	CamelContextFactoryBean camelContext() {
@@ -24,11 +26,20 @@ public class CwatchServiceMain implements CommandLineRunner {
 		return factory;
 	}
 	
+	@Bean
+	ActiveMQComponent activemq(ConnectionFactory connectionFactory) {
+		ActiveMQConfiguration cfg = new ActiveMQConfiguration();
+		cfg.setConnectionFactory(connectionFactory);
+		cfg.setErrorHandlerLogStackTrace(false);
+		cfg.setUsePooledConnection(false);
+		
+		ActiveMQComponent cmp = new ActiveMQComponent();
+		cmp.setConfiguration(cfg);
+		return cmp;
+	}
+	
 	@Override
 	public void run(String... args) throws Exception {
-		Main main = new Main();
-		main.setApplicationContext(applicationContext);
-		main.run();
 	}
 
 	public static void main(String[] args) {
--- core/src/main/java/org/cwatch/service/routes/CwatchRouteBuilder.java
+++ core/src/main/java/org/cwatch/service/routes/CwatchRouteBuilder.java
@@ -1,10 +1,6 @@
 package org.cwatch.service.routes;
 
-import org.apache.activemq.camel.component.ActiveMQComponent;
-import org.apache.activemq.camel.component.ActiveMQConfiguration;
 import org.apache.camel.spring.SpringRouteBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,17 +18,6 @@ public class CwatchRouteBuilder extends SpringRouteBuilder {
 		from("activemq:topic:stires.vdm.batch?clientId=cwatchServiceBatch")
 		.id("batchReceiver")
 		.to("log:batch?showBody=false");
-	}
-	
-	
-	@Bean
-	ActiveMQComponent activemq(@Value("${cwatch-service.split.brokerUrl:tcp://tstarback1.emsa.local:61616}") String brokerUrl) {
-		ActiveMQConfiguration cfg = new ActiveMQConfiguration();
-		cfg.setBrokerURL(brokerUrl);
-		
-		ActiveMQComponent cmp = new ActiveMQComponent();
-		cmp.setConfiguration(cfg);
-		return cmp;
 	}
 	
 }
