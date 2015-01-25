4d7ba8b103818cb9710c2e9e9345e192002785c0
Marton Papp <maprohu@gmail.com>
2015-01-25 12:58:28 +0000

    error handling for failed ais2cdf (in progress)

--- core/src/main/java/org/cwatch/service/routes/AisToCdfRouteBuilder.java
+++ core/src/main/java/org/cwatch/service/routes/AisToCdfRouteBuilder.java
@@ -3,11 +3,12 @@ package org.cwatch.service.routes;
 import java.lang.reflect.Type;
 
 import org.apache.camel.component.gson.GsonDataFormat;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.cwatch.service.CwatchServiceProperties;
+import org.cwatch.vdm.cdf.AisMessageToCdfConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
 import ssn.spm.domain.ais.AisMessageContainer;
@@ -18,11 +19,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 @Component
+@Import(AisMessageToCdfConverter.class)
 @EnableConfigurationProperties(CwatchServiceProperties.class)
 public class AisToCdfRouteBuilder extends SpringRouteBuilder {
 
 	@Autowired
 	CwatchServiceProperties configuration;
+	
+	@Autowired
+	AisMessageToCdfConverter ais2cdf;
 	
 	@Override
 	public void configure() throws Exception {
@@ -37,23 +42,38 @@ public class AisToCdfRouteBuilder extends SpringRouteBuilder {
 		.id("ais2cdf")
 		.unmarshal().gzip()
 		.unmarshal(aisgson)
+		.setProperty("aisMessageId").simple("${body.messageId}")
 		.split(simple("${body.aisMessages}"))
-			.setHeader("aisMessageType", simple("${body.positionReport.aisMessageType}"))
-		.choice()
-			.when(header("aisMessageType").isEqualTo(5))
-				.to("direct:ais2cdfVoyage")
-			.when(header("aisMessageType").in(1,2,3,18.19))
-				.to("direct:ais2cdfPosition")
-			.otherwise()
-				.stop()
-		.endChoice()
-		.to("log:ais2cdf?showAll=false&showBody=false");
-		
-		from("direct:ais2cdfVoyage")
-		.to("log:ais2cdfVoyage?showAll=false&showBody=false");
+			.doTry()
+				.setProperty("aisMessageType").ognl("@java.lang.Integer@valueOf(request.body.positionReport.aisMessageType)")
+				.choice()
+					.when(property("aisMessageType").isEqualTo(5))
+						.to("direct:ais2cdfVoyage")
+					.when(property("aisMessageType").in(1,2,3,18.19))
+						.to("direct:ais2cdfPosition")
+					.otherwise()
+						.to("direct:ais2cdfOther")
+				.endDoTry()
+			.doCatch(Exception.class)
+				.to("direct:ais2cdfError")
+			.end()
+		;
 		
 		from("direct:ais2cdfPosition")
+		.bean(ais2cdf, "convertPosition")
 		.to("log:ais2cdfPosition?showAll=false&showBody=false");
+		
+		from("direct:ais2cdfVoyage")
+		.bean(ais2cdf, "convertVoyage")
+		.to("log:ais2cdfVoyage?showAll=false&showBody=false");
+		
+		from("direct:ais2cdfOther")
+		.to("log:ais2cdfOther?showAll=false&showBody=false");
+		
+		from("direct:ais2cdfError")
+		.to("log:ais2cdfError?showAll=false&showBody=false");
+		
+		
 	}
 	
 }
