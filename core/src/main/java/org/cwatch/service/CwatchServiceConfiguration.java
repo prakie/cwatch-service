810908621b4d1e8f0a9fe459782726456c89941f
maprohu <maprohu@gmail.com>
2015-02-19 15:54:25 +0000

    import routes

--- core/src/main/java/org/cwatch/service/CwatchServiceConfiguration.java
+++ core/src/main/java/org/cwatch/service/CwatchServiceConfiguration.java
@@ -1,11 +1,15 @@
 package org.cwatch.service;
 
+import org.cwatch.service.routes.AisToCdfRouteBuilder;
+import org.cwatch.service.routes.CwatchRouteBuilder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources(@PropertySource("classpath:/cwatch-service.properties"))
+@Import({CwatchRouteBuilder.class, AisToCdfRouteBuilder.class})
 public class CwatchServiceConfiguration {
 
 }
